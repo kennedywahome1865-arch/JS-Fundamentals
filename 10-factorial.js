14159511128949d9d71a322042085a60b69e32d4
kennedywahome1865-arch <kennedywahome1865@gmail.com>
2025-11-06 04:09:12 +0300

    Fix factorial of NaN to correctly return 1

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,5 +1,5 @@
-function factorial (n) {
-  if (n <= 1) {
+function factorial(n) {
+  if (isNaN(n) || n <= 1) {
     return 1;
   }
   return n * factorial(n - 1);
@@ -7,3 +7,4 @@ function factorial (n) {
 
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
+
